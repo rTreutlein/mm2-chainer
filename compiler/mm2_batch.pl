901fbfe3d02e1ad1e93f87cdc339807385334a3f
Roman Treutlein <roman@trueagi.io>
2026-07-21 17:57:24 +0200

    Revert "Parallelize MM2 batch serialization"
    This reverts commit 99d27f475e203322f61c10f8fde62752439d74a2.

--- compiler/mm2_batch.pl
+++ compiler/mm2_batch.pl
@@ -1,7 +1,5 @@
 :- module(mm2_batch, [mork_add_atoms/2]).
 
-:- use_module(library(thread)).
-
 metta_cons_list([], []).
 metta_cons_list([cons, Head, Tail], [Head|Rest]) :-
     !,
@@ -9,16 +7,9 @@ metta_cons_list([cons, Head, Tail], [Head|Rest]) :-
 metta_cons_list(List, List) :-
     is_list(List).
 
-serialize_mork_atoms(Atoms, Texts) :-
-    length(Atoms, Count),
-    ( Count >= 8
-    -> concurrent_maplist(swrite, Atoms, Texts)
-    ;  maplist(swrite, Atoms, Texts)
-    ).
-
 mork_add_atoms(MettaAtoms, true) :-
     metta_cons_list(MettaAtoms, Atoms),
-    serialize_mork_atoms(Atoms, Texts),
+    maplist(swrite, Atoms, Texts),
     atomics_to_string(Texts, " ", Source),
     mork("add-atoms", Source, Result),
     sub_string(Result, 0, 2, _, "OK").
